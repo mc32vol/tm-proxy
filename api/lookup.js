cc1f4de2d33aebbd72c527a596c19f6a22c67d10
mc32vol <39711543+mc32vol@users.noreply.github.com>
2026-05-17 13:42:37 -0700

    Update lookup.js

--- api/lookup.js
+++ api/lookup.js
@@ -1,28 +1,9 @@
 module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  if (req.method === 'OPTIONS') {
-    return res.status(204).end();
-  }
-
   const { num, type } = req.query;
-  if (!num || !type) {
-    return res.status(400).json({ error: 'Missing num or type' });
-  }
-
-  try {
-    const field = type === 'rn' ? 'registration_number' : 'serial_number';
-    const url = `https://markbase.co/trademarks?${field}=${num}`;
-
-    const response = await fetch(url, {
-      headers: { 'Accept': 'application/json' }
-    });
-
-    const text = await response.text();
-    return res.status(200).send(text);
-  } catch (err) {
-    return res.status(500).json({ error: err.message });
-  }
+  return res.status(200).json({ 
+    message: 'Proxy is working!', 
+    num: num, 
+    type: type 
+  });
 };
